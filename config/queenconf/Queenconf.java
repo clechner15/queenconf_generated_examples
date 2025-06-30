313a82ebe4932aaf026087ccc2651c03a251c913
Christian Lechner <di.lechner@gmail.com>
2025-06-30 10:22:43 +0200

    Update queenconf files

--- config/queenconf/Queenconf.java
+++ config/queenconf/Queenconf.java
@@ -5,22 +5,22 @@ public class Queenconf {
 
     public class Config {
         public class Admin {
-  public final String adminPageIconName = "user"; // name of icon for admin page
+          public final String adminPageIconName = "user"; // name of icon for admin page
         }
 
         public final Admin admin = new Admin();
 
         public class Laywer {
-  public final String codeOfConductContent = "Every participant has to...."; // content of code of conduct
-  public final String extraPageTitle = "\"\""; // show the tiltle of the extra page
+          public final String codeOfConductContent = "Every participant has to...."; // content of code of conduct
+          public final String extraPageTitle = "\"\""; // show the tiltle of the extra page
         }
 
         public final Laywer laywer = new Laywer();
 
-  public final boolean showExtraHeader = true; // show extra header
-  public final int rowsPerPage = 4; // how many rows per page are shown
-  public final boolean userMenuVisible = false; // the user menu should be shown or hidden
-  public final boolean showBlackFridayBanner = true; // show black friday banner
+          public final boolean showExtraHeader = true; // show extra header
+          public final int rowsPerPage = 4; // how many rows per page are shown
+          public final boolean userMenuVisible = false; // the user menu should be shown or hidden
+          public final boolean showBlackFridayBanner = true; // show black friday banner
     }
 
 }
